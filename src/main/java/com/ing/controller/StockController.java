a87cbc724087db5e0c222a31d939f202cd774d22
bandisravan <sravan10771@gmail.com>
2019-04-07 17:58:41 +0530

    Day Count committing

--- src/main/java/com/ing/controller/StockController.java
+++ src/main/java/com/ing/controller/StockController.java
@@ -126,4 +126,80 @@ public class StockController {
 	    
 		return map;
 	}
+	
+	@GetMapping("/dayCount")
+	public Map getDayCount() {
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date1 = null,d1,d2;
+		int count=0;
+		List<StockCount> stockcountlist = new ArrayList<StockCount>();
+		long diff,diffHours = 0;
+		Calendar calobj = Calendar.getInstance();
+		//String sDate1="2019/04/07 13:48:17";  //this string will come from db
+		String sDate1 = null;
+		StockCount stockcountObj=new StockCount();
+		try {
+			for(ReviewHistory reviewhistory : transactionService.getAllHistory()) {
+				sDate1=reviewhistory.getTradedTime();
+				date1 = format.parse(sDate1);
+				d1 = format.parse(df.format(date1));
+				d2 = format.parse(df.format(calobj.getTime()));
+				diff = d2.getTime() - d1.getTime();
+				diffHours = diff / (60 * 60 * 1000) % 24;
+				boolean status=false;
+				if(diffHours<=24) {
+					if(stockcountlist.size()==0) {
+						stockcountObj=new StockCount();
+						stockcountObj.setCount(count+1);
+						stockcountObj.setQuantity(reviewhistory.getQuantity());
+						stockcountObj.setStockname(reviewhistory.getStockName());
+						stockcountlist.add(stockcountObj);
+						//System.out.println("stockcountlist  "+reviewhistory.getStockName());
+						//System.out.println("count  "+stockcountObj.getCount());
+					}else {
+						
+						for(int i=0;i<stockcountlist.size();i++) {
+							//System.out.println("else part "+stockcountlist.get(i).getStockname());
+							stockcountObj=stockcountlist.get(i);
+							System.out.println(stockcountlist.contains(reviewhistory.getStockName()));
+							if(stockcountlist.contains(reviewhistory.getStockName()))
+								System.out.println("cont");
+							else
+								System.out.println("not");
+							if(stockcountObj.getStockname().equals(reviewhistory.getStockName())){
+								//System.out.println(reviewhistory.getStockName()+"  equal  "+stockcountlist.get(i).getStockname());
+								int temp=stockcountObj.getCount();
+								int qua=stockcountObj.getQuantity();
+								stockcountObj.setCount(temp+1);
+								stockcountObj.setQuantity(reviewhistory.getQuantity()+qua);
+								stockcountlist.remove(i);
+								stockcountlist.add(stockcountObj);
+								status=true;
+							}
+						}
+						if(!status) {
+							//System.out.println("second else "+stockcountlist.get(i).getStockname());
+							stockcountObj=new StockCount();
+							count=0;
+							stockcountObj.setCount(count+1);
+							stockcountObj.setStockname(reviewhistory.getStockName());
+							stockcountObj.setQuantity(reviewhistory.getQuantity());
+							stockcountlist.add(stockcountObj);
+							
+						}
+					}
+						
+					
+				}
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} 
+	    
+	    Map map =new HashMap();
+	    map.put("stockcountlist",stockcountlist);
+	    
+		return map;
+	}
 }
